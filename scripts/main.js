b3647466a94e11ea89b0aa75ed937256d0d66f87
yagoluucas <yago543@gmail.com>
2024-01-27 11:41:09 -0300

    arrumado a posição dos elementos e feito escopo inicial de designer

--- scripts/main.js
+++ scripts/main.js
@@ -6,38 +6,40 @@ window.addEventListener('DOMContentLoaded', () => {
     const data = new Date()
     const sectionApagarAnotacao = document.querySelector('.js-section-apagar-anotacao')
     const btnSectionApagarAnotacao = document.querySelectorAll('.js-section-apagar-anotacao button')
+    const header = document.querySelector('header')
+    function removerFundoApagarAnotacao() {
+        sectionApagarAnotacao.classList.add('none')
+        document.body.classList.remove('escurecer-fundo')
+        main.classList.remove('deixar-transparente')
+        header.classList.remove('deixar-transparente')
+    }
 
     function apagarAnotacao(lixeiraClicado) {
-
         function confirmaApagarAnotacao() {
             btnSectionApagarAnotacao.forEach((btn) => btn.removeEventListener('click', confirmaApagarAnotacao))
-            switch(this.textContent) {
+            switch (this.textContent) {
                 case "Sim":
-                    console.log(lixeiraClicado.target.parentElement)
                     main.removeChild(lixeiraClicado.target.parentElement)
                     btnSectionApagarAnotacao.forEach((btn) => btn.classList.add('none'))
                     sectionApagarAnotacao.firstElementChild.textContent = 'Anotação apagada'
                     setTimeout(() => {
                         sectionApagarAnotacao.firstElementChild.textContent = 'Deseja Realmente apagar a anotação ?'
                         btnSectionApagarAnotacao.forEach((btn) => btn.classList.remove('none'))
-                        sectionApagarAnotacao.classList.add('none')
-                        document.body.classList.remove('escurecer-fundo')
-                        main.classList.remove('deixar-transparente')
+                        removerFundoApagarAnotacao()
                     }, 1500)
-                break
+                    break
 
                 case "Não":
-                    sectionApagarAnotacao.classList.add('none')
-                    document.body.classList.remove('escurecer-fundo')
-                    main.classList.remove('deixar-transparente')
-                break
+                    removerFundoApagarAnotacao()
+                    break
             }
         }
 
-
         document.body.classList.add('escurecer-fundo')
         main.classList.add('deixar-transparente')
+        sectionApagarAnotacao.classList.add('animacao-apagar-anotacao')
         sectionApagarAnotacao.classList.remove('none')
+        header.classList.add('deixar-transparente')
         btnSectionApagarAnotacao.forEach((btn) => btn.addEventListener('click', confirmaApagarAnotacao))
 
     }
@@ -47,11 +49,11 @@ window.addEventListener('DOMContentLoaded', () => {
         const dataAnotacao = document.createElement('p')
         dataAnotacao.textContent = `data : ${data.getDate()}/${mesFormatado}/${data.getFullYear()}`
         dataAnotacao.classList.add('data-anotacao')
-        const imgLixeira = document.createElement('img')
+        let imgLixeira = document.createElement('img')
         imgLixeira.setAttribute('src', '../image/lixeira.svg')
         imgLixeira.setAttribute('alt', 'lixeira')
         imgLixeira.classList.add('img-lixeira', 'js-img-lixeira')
-        const sectionAnotacao = document.createElement('section')       
+        const sectionAnotacao = document.createElement('section')
         const paragrafoAnotacao = document.createElement('p')
         paragrafoAnotacao.classList.add('anotacao')
         paragrafoAnotacao.textContent = inputTextoAnotacao.value
@@ -63,7 +65,16 @@ window.addEventListener('DOMContentLoaded', () => {
         imgLixeira.addEventListener('click', apagarAnotacao)
     }
 
-    
-    btnGerarAnotacao.addEventListener('click', gerarAnotacao)
+
+    inputTextoAnotacao.addEventListener('keyup', () => {
+        if (inputTextoAnotacao.checkValidity()) {
+            btnGerarAnotacao.addEventListener('click', gerarAnotacao)
+            btnGerarAnotacao.removeAttribute('disabled')
+        } else {
+            btnGerarAnotacao.setAttribute('disabled', 'true')
+            btnGerarAnotacao.removeEventListener('click', gerarAnotacao)
+        }
+    })
+
 
 })
