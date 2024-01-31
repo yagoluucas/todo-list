24e39610458cb2a3f8b45e3e3b422e3321231cb1
yagoluucas <yago543@gmail.com>
2024-01-31 11:22:26 -0300

    mudado o layout do projeto

--- scripts/main.js
+++ scripts/main.js
@@ -6,7 +6,7 @@ window.addEventListener('DOMContentLoaded', () => {
     const btnSectionApagarAnotacao = document.querySelectorAll('.js-section-apagar-anotacao button')
     const inputTextoAnotacao = document.querySelector('.js-texto-anotacao')
     const btnGerarAnotacao = document.querySelector('.js-btn-gerar-anotacao')
-    const data = new Date()
+    const containerAnotacoes = document.querySelector('.js-anotacoes')
     const header = document.querySelector('header')
     if (localStorage.getItem('idsAnotacao') == null || localStorage.getItem('quantidadeDeAnotacoes') == null) {
         quantidadeDeAnotacoes = 0
@@ -18,28 +18,24 @@ window.addEventListener('DOMContentLoaded', () => {
         const anotacoes = JSON.parse(localStorage.getItem('idsAnotacao'))
         anotacoes.forEach((id) => {
             let infoAnotacao = JSON.parse(localStorage.getItem(`anotacao ${id}`))
-            gerarSection(infoAnotacao[0], infoAnotacao[1], infoAnotacao[2])
+            gerarSection(infoAnotacao[0], infoAnotacao[1])
         })
     }
 
-    function gerarSection(textoAnotacao, textoDataAnotacao, idAnotacao) {
+    function gerarSection(textoAnotacao,idAnotacao) {
         const sectionAnotacao = document.createElement('section')
         sectionAnotacao.classList.add('section-anotacao')
         sectionAnotacao.setAttribute('data-idAnotacao', idAnotacao)
-        const dataAnotacao = document.createElement('p')
         const paragrafoAnotacao = document.createElement('p')
         paragrafoAnotacao.textContent = textoAnotacao
         paragrafoAnotacao.classList.add('anotacao')
-        dataAnotacao.classList.add('data-anotacao')
-        dataAnotacao.textContent = textoDataAnotacao
-        sectionAnotacao.appendChild(dataAnotacao)
         sectionAnotacao.appendChild(paragrafoAnotacao)
         let imgLixeira = document.createElement('img')
         imgLixeira.setAttribute('src', './image/lixeira.svg')
         imgLixeira.setAttribute('alt', 'lixeira')
         imgLixeira.classList.add('img-lixeira', 'js-img-lixeira')
         sectionAnotacao.appendChild(imgLixeira)
-        main.insertBefore(sectionAnotacao, main.firstChild)
+        containerAnotacoes.insertBefore(sectionAnotacao, containerAnotacoes.firstChild)
         imgLixeira.addEventListener('click', apagarAnotacao)
         let arrayComId = JSON.parse(localStorage.getItem('idsAnotacao'))
         if (!(arrayComId.includes(idAnotacao))) {
@@ -61,7 +57,7 @@ window.addEventListener('DOMContentLoaded', () => {
                         quantidadeDeAnotacoes--
                         localStorage.setItem('quantidadeDeAnotacoes', quantidadeDeAnotacoes)
                     }
-                    main.removeChild(lixeiraClicado.target.parentElement)
+                    containerAnotacoes.removeChild(lixeiraClicado.target.parentElement)
                     localStorage.removeItem(`anotacao ${idAnotacaoRemovida}`)
                     btnSectionApagarAnotacao[0].parentElement.classList.add('none')
                     sectionApagarAnotacao.firstElementChild.textContent = 'Anotação apagada'
@@ -98,13 +94,14 @@ window.addEventListener('DOMContentLoaded', () => {
     function gerarAnotacao() {
         quantidadeDeAnotacoes++
         idAnotacao++
-        const mesFormatado = data.getMonth() + 1 < 10 ? '0'.concat(`${data.getMonth() + 1}`) : data.getMonth() + 1
-        const textoData = `data da anotação : ${data.getDate()}/${mesFormatado}/${data.getFullYear()}`
-        gerarSection(inputTextoAnotacao.value, textoData, idAnotacao)
-        const anotacaoInfo = [inputTextoAnotacao.value, textoData, idAnotacao]
+        // const mesFormatado = data.getMonth() + 1 < 10 ? '0'.concat(`${data.getMonth() + 1}`) : data.getMonth() + 1
+        // const textoData = `data da anotação : ${data.getDate()}/${mesFormatado}/${data.getFullYear()}`
+        gerarSection(inputTextoAnotacao.value, idAnotacao)
+        const anotacaoInfo = [inputTextoAnotacao.value, idAnotacao]
         localStorage.setItem(`anotacao ${idAnotacao}`, JSON.stringify(anotacaoInfo))
         localStorage.quantidadeDeAnotacoes = quantidadeDeAnotacoes
         localStorage.setItem('ultimoId', idAnotacao)
+        inputTextoAnotacao.value = ''
     }
 
     inputTextoAnotacao.addEventListener('keydown', (e) => {
